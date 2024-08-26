ea0967c6e79bec939d1b4c3110bf4a3a7703b2a9
Aditya Bose <boseaditya680@gmail.com>
2024-08-26 22:17:03 +0530

    UI DONE

--- Join.jsx
+++ Join.jsx
@@ -1,14 +1,14 @@
 //This file is for join voice-Text and Text-voice
 
 import * as React from "react";
-import { NavigationContainer } from "@react-navigation/native"; 
-import { createStackNavigator } from "@react-navigation/stack"; 
-import VoiceTranslator from "./VoiceTranslator";
+import { NavigationContainer } from "@react-navigation/native"; // for navigate
+import { createStackNavigator } from "@react-navigation/stack";   // for one page to another page
+import VoiceTranslator from "./VoiceTranslator"; 
 import TextToVoice from "./TextToVoice";
 
 const Stack = createStackNavigator();       // create stack
 
-export default function App() {         // first navigator = voiceTranslator
+export default function App() {         // Initial navigator = voiceTranslator
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="VoiceTranslator">   
